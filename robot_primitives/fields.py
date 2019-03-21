56413e2fefb36d5095c0c64846b388ef7789b2b9
Chris Tomaszewski <christomaszewski@gmail.com>
2019-03-21 16:40:02 -0400

    Changed flow definition to avoid confusion with tuples.

--- robot_primitives/fields.py
+++ robot_primitives/fields.py
@@ -19,15 +19,15 @@ class VectorField(Field):
 
 	@classmethod
 	def from_channel_flow_with_pylon(cls, channel_width, max_velocity, pylon_bounds):
-		channel_flow = lambda x,y: (0, ((4 * x / channel_width - 4 * x**2 / channel_width**2) * max_velocity))
+		channel_flow = lambda x: (4 * x / channel_width - 4 * x**2 / channel_width**2) * max_velocity
 
 		pylon_width = pylon_bounds[1] - pylon_bounds[0]
 
-		pylon_boundary_flow = [channel_flow(x, 0) for x in pylon_bounds]
+		pylon_boundary_flow = [channel_flow(x) for x in pylon_bounds]
 
 		pylon_flow = lambda x: pylon_boundary_flow[0] - 2*x*pylon_boundary_flow[0]/pylon_width if x < pylon_bounds[0] + pylon_width/2 else 2*x*pylon_boundary_flow[1]/pylon_width
 
-		compound_flow = lambda x,y: channel_flow(x,y) if x < pylon_bounds[0] or x > pylon_bounds[1] else (0, pylon_flow(x))
+		compound_flow = lambda x,y: (0, channel_flow(x)) if x < pylon_bounds[0] or x > pylon_bounds[1] else (0, pylon_flow(x))
 
 		return cls(compound_flow)
 
